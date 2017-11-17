4bb0887e0cfae33a25021fbf03921241f3e05ea3
vladwoguer.bezerra <vladwoguer.bezerra@virtus.ufcg.edu.br>
2017-11-16 21:41:55 -0300

    First Program

---
+++ compile.sh
@@ -0,0 +1,3 @@
+read -p "Nome do programa: " nome
+cobc -free -x -o $nome $nome".cbl"
+
---
+++ ola_mundo.cbl
@@ -0,0 +1,6 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. OLA-MUNDO.
+      *> Exemplo COBOL Vladwoguer Bezerra Nov-2017
+       PROCEDURE DIVISION.
+	   DISPLAY 'OLÁ MUNDO!'.
+	   STOP RUN.
